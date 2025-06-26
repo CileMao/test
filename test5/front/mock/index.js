34a7737d63bd0a12872ae7125d512c9dab1a52c4
CileMao <787421225@qq.com>
2025-06-26 22:58:05 +0800

    Delete test5 directory

--- test5/front/mock/index.js
+++
@@ -1,9 +0,0 @@
-import Mock from 'mockjs'
-
-Mock.mock('/api/soil/properties', 'get', {
-  'data|10': [{
-    'id|+1': 1,
-    'ph': '@float(3, 8, 1, 1)',
-    'organic': '@float(0.5, 5, 1, 1)'
-  }]
-})
